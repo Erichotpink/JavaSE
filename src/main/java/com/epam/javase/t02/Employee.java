5ea1acecc599179990ff4d6e2450dd7c89108cd8
Andrey <mailtoivanovandrey@gmail.com>
2017-02-27 00:46:43 +0300

    Task 2 initial version

---
+++ src/main/java/com/epam/javase/t02/Employee.java
@@ -0,0 +1,26 @@
+package com.epam.javase.t02;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by aivanov on 2/27/2017.
+ */
+public class Employee {
+
+    private static int lastID = 0;
+
+    private final String name;
+    private final String surname;
+    private final int id;
+    private final List stationeris = Collections.;
+
+    public Employee(String name, String surname) {
+    }
+
+    public Employee(String name, String surname, List stationeris) {
+        this.name = name;
+        this.surname = surname;
+
+    }
+}
---
+++ src/main/java/com/epam/javase/t02/Stationery.java
@@ -0,0 +1,7 @@
+package com.epam.javase.t02;
+
+/**
+ * Created by aivanov on 2/27/2017.
+ */
+public class Stationery {
+}
