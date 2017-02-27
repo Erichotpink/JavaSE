274b5d3a808b77655b45630c1a644f5261a6dc68
Andrey <mailtoivanovandrey@gmail.com>
2017-02-27 10:26:18 +0300

    Stationery class completed.

--- src/main/java/com/epam/javase/t02/Stationery.java
+++ src/main/java/com/epam/javase/t02/Stationery.java
@@ -1,7 +1,37 @@
 package com.epam.javase.t02;
 
+import java.util.Objects;
+
 /**
  * Created by aivanov on 2/27/2017.
  */
 public class Stationery {
+
+    private String name;
+    private double cost;
+
+    public Stationery(String name, double cost) {
+        Objects.requireNonNull(name, "Name cannot be null.");
+        if (Double.compare(cost, 0.0) < 0) {
+            throw new IllegalArgumentException("Cost must be positive. Specified value: " + cost);
+        }
+
+        this.name = name;
+        this.cost = cost;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) return true;
+        if (other == null || getClass() != other.getClass()) return false;
+
+        Stationery obj = (Stationery) other;
+
+        return (Double.compare(this.cost, obj.cost) == 0)
+                && this.name.equals(obj.name);
+    }
 }
