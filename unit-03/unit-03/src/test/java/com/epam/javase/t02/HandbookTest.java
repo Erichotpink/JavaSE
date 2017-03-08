a984bc02437f33e7ccba09ec2b02d01acedfc752
Andrey <mailtoivanovandrey@gmail.com>
2017-03-08 22:43:34 +0300

    u03t02 - Test methods fixed

--- unit-03/unit-03/src/test/java/com/epam/javase/t02/HandbookTest.java
+++ unit-03/unit-03/src/test/java/com/epam/javase/t02/HandbookTest.java
@@ -2,48 +2,32 @@ package com.epam.javase.t02;
 
 import org.junit.Test;
 
+import java.util.Set;
+
 import static org.junit.Assert.*;
 
 /**
  * Created by aivanov on 3/8/2017.
  */
 public class HandbookTest {
+
+    private final Handbook handbook = new Handbook();
+    private final Set<String> set = handbook.getQuestionsIDs();
+
     @Test
     public void testGetQuestionsIDsReturnNotEmptyData() throws Exception {
-        Handbook handbook = new Handbook();
-
         assertFalse(handbook.getQuestionsIDs().isEmpty());
         handbook.getQuestionsIDs().stream().forEach(s -> assertFalse(s.isEmpty()));
     }
 
     @Test
-    public void getQuestionByID() throws Exception {
-
+    public void testGetQuestionByIDReturnNotEmptyData() throws Exception {
+        set.stream().forEach(s -> assertFalse(handbook.getQuestionByID(s).isEmpty()));
     }
 
     @Test
-    public void getAnswer1() throws Exception {
+    public void testIfWeHaveAnswerForEachQuestions() throws Exception {
+        set.stream().forEach(s -> assertFalse((handbook.getAnswer(s).isEmpty())));
 
     }
-
-    @Test
-    public void changeLocale() throws Exception {
-
-    }
-
-    @Test
-    public void getQuestions() throws Exception {
-        Handbook data = new Handbook();
-
-//        assertTrue(data.getQuestions().size() != 0);
-//        data.getQuestions().stream().forEach(s -> assertFalse(s.isEmpty()));
-    }
-
-    @Test
-    public void getAnswer() throws Exception {
-        Handbook data = new Handbook();
-
-//        data.getQuestions().stream().forEach(s -> assertFalse(data.getAnswer(s).isEmpty()));
-    }
-
 }
