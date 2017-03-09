b6124c1d1198512b53ca5096d792a696546b5690
Andrey <mailtoivanovandrey@gmail.com>
2017-03-09 04:24:32 +0300

    u03t03 - Added method to search for sentences

--- unit-03/unit-03/src/main/java/com/epam/javase/t03/ArticleParser.java
+++ unit-03/unit-03/src/main/java/com/epam/javase/t03/ArticleParser.java
@@ -1,20 +1,60 @@
 package com.epam.javase.t03;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by aivanov on 3/8/2017.
  */
 public class ArticleParser {
 
-    private final InputStream stream = getClass().getResourceAsStream("resources/com.epam.javase.t03/article.html");
+    private final InputStream stream = getClass().getResourceAsStream(
+            "/com/epam/javase/t03/test.html");
 
-    public ArticleParser() {
-        System.out.println(stream == null);
+    private String getContent() {
+
+        try(ByteArrayOutputStream result = new ByteArrayOutputStream()) {
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = stream.read(buffer)) != -1) {
+                result.write(buffer, 0, length);
+            }
+            return result.toString("Windows-1251");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        return "";
+    }
+
+    public List<String> searchForSentencesWithPictures() {
+        List<String> data = new ArrayList<>();
+
+        String article = getContent();
+
+        String pattern = "[А-Я][^.!?]*(?<=(\\([Рр]ис)).+?[.!?]";
+
+        Pattern p = Pattern.compile(pattern, Pattern.UNICODE_CHARACTER_CLASS);
+        Matcher m = p.matcher(article);
+
+        while(m.find()) {
+            data.add(m.group());
+        }
+
+        return data;
     }
 
     public static void main(String[] args) {
         ArticleParser temp = new ArticleParser();
-
+        temp.searchForSentencesWithPictures().stream().forEach( s -> {
+            System.out.println("-----------");
+            System.out.println(s);
+        });
     }
+
 }
