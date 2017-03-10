fdcf610881a0b115e1a00480dedb5b88380c1fe9
Andrey <mailtoivanovandrey@gmail.com>
2017-03-11 02:42:00 +0300

    u04t01 - Added methods to count word statistics

--- unit-04/src/main/java/com/epam/javase/t01/KeywordsReader.java
+++ unit-04/src/main/java/com/epam/javase/t01/KeywordsReader.java
@@ -1,7 +1,50 @@
 package com.epam.javase.t01;
 
+import java.io.*;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
+ * Represents a class to get statistics of usage java keywords in the specified file.
+ *
  * Created by aivanov on 3/10/2017.
  */
 public class KeywordsReader {
+
+    public static void getKeywordsUsageStat(File input, File output) {
+
+        try (InputStream in = new FileInputStream(input);
+            OutputStream out = new FileOutputStream(output)) {
+
+        } catch (IOException ex) {
+        }
+    }
+
+    public static String countKeywordsStat(String str) {
+        StringBuffer data = new StringBuffer();
+        Set<String> set = JavaKeywords.getJavaKeywords();
+
+        for (String s : set) {
+            int result = KeywordsReader.countUsageStat(s, str);
+            data.append(s + ":" + result + ";");
+        }
+
+        return data.toString();
+    }
+
+    public static int countUsageStat(String keyword, String str) {
+        String pattern = "(\\b)"+ keyword + "(\\b)";
+        int count = 0;
+
+        Pattern p = Pattern.compile(pattern);
+        Matcher m = p.matcher(str);
+
+        while (m.find()) {
+            count++;
+        }
+
+        return count;
+    }
 }
+
