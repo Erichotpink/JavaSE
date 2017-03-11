f60fcbd2f7309ea950c8d5bb3b1d3204e3e88b69
Andrey <mailtoivanovandrey@gmail.com>
2017-03-11 04:14:49 +0300

    u04t01 - Working option(tests and last method completed)

--- unit-04/src/main/java/com/epam/javase/t01/KeywordsReader.java
+++ unit-04/src/main/java/com/epam/javase/t01/KeywordsReader.java
@@ -12,27 +12,60 @@ import java.util.regex.Pattern;
  */
 public class KeywordsReader {
 
+    /**
+     * Reads the source file and writes keyword usage statistics to the specified output file.
+     *
+     * @param input source file
+     * @param output output file
+     */
     public static void getKeywordsUsageStat(File input, File output) {
 
-        try (InputStream in = new FileInputStream(input);
-            OutputStream out = new FileOutputStream(output)) {
+        try (InputStream in = new BufferedInputStream(new FileInputStream(input));
+            OutputStream out = new BufferedOutputStream(new FileOutputStream(output))) {
+
+            String data = "";
+            byte[] buffer = new byte[1024];
+            int bytesRead = 0;
+
+            while((bytesRead = in.read(buffer)) > 0) {
+                data += new String(buffer, 0, bytesRead);
+            }
+
+            byte[] result = countKeywordsStat(data).getBytes();
+            out.write(result);
 
         } catch (IOException ex) {
+            ex.printStackTrace();
         }
     }
 
+    /**
+     * Count keywords usage statistics for the specified string.
+     *
+     * @param str string to be parsed
+     * @return String with results in the following format 'keyword(String):count(int);'
+     */
     public static String countKeywordsStat(String str) {
         StringBuffer data = new StringBuffer();
         Set<String> set = JavaKeywords.getJavaKeywords();
 
         for (String s : set) {
             int result = KeywordsReader.countUsageStat(s, str);
-            data.append(s + ":" + result + ";");
+            if (result > 0) {
+                data.append(s + ":" + result + ";");
+            }
         }
 
         return data.toString();
     }
 
+    /**
+     * Parse the specified string and count occurrences of the specified keyword.
+     *
+     * @param keyword keyword to be counted
+     * @param str source to be parsed
+     * @return number of occurrences
+     */
     public static int countUsageStat(String keyword, String str) {
         String pattern = "(\\b)"+ keyword + "(\\b)";
         int count = 0;
--- unit-04/src/test/java/com/epam/javase/t01/KeywordsReaderTest.java
+++ unit-04/src/test/java/com/epam/javase/t01/KeywordsReaderTest.java
@@ -2,6 +2,8 @@ package com.epam.javase.t01;
 
 import org.junit.Test;
 
+import java.io.File;
+
 import static org.junit.Assert.*;
 
 /**
@@ -9,12 +11,50 @@ import static org.junit.Assert.*;
  */
 public class KeywordsReaderTest {
     @Test
-    public void countKeywordsStat() throws Exception {
+    public void TestIfResultFileCreated() throws Exception {
+        String src = "C:/Temp/KeywordsReader.java";
+        String dst = "C:/Temp/Result.txt";
 
+        File source = new File(src);
+        File dest = new File(dst);
+
+        KeywordsReader.getKeywordsUsageStat(source, dest);
+
+        assertTrue(dest.exists());
     }
 
     @Test
-    public void PositiveAndNegativeCountUsageStatTests() throws Exception {
+    public void TestIfEmptyResultFileCreated() throws Exception {
+        String src = "C:/Temp/Empty.java";
+        String dst = "C:/Temp/Empty.txt";
+
+        File source = new File(src);
+        source.createNewFile();
+        File dest = new File(dst);
+
+        KeywordsReader.getKeywordsUsageStat(source, dest);
+
+        assertTrue(dest.exists());
+    }
+
+    @Test
+    public void testDataFormatPositiveCase() throws Exception {
+        String str = "public void main(String... args)";
+        String pattern = "public:1;void:1;";
+
+        assertTrue(KeywordsReader.countKeywordsStat(str).equals(pattern));
+    }
+
+    @Test
+    public void testDataFormatNegativeCase() throws Exception {
+        String str = "publicd dvoid main(String... args)";
+        String pattern = "";
+
+        assertTrue(KeywordsReader.countKeywordsStat(str).equals(pattern));
+    }
+
+    @Test
+    public void testResultStatisticsPositiveAndNegativeCases() throws Exception {
 
         String str = "public static void main(String... args)";
 
