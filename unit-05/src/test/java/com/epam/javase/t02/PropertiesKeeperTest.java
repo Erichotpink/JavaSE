056db3e3ac5e5f2f13b3d1de1e2ec4395555f240
Andrey <mailtoivanovandrey@gmail.com>
2017-03-15 02:18:51 +0300

    u05t02 - Fix tests. Task completed.

--- unit-05/src/test/java/com/epam/javase/t02/PropertiesKeeperTest.java
+++ unit-05/src/test/java/com/epam/javase/t02/PropertiesKeeperTest.java
@@ -13,30 +13,39 @@ import static org.junit.Assert.*;
 public class PropertiesKeeperTest {
 
     File file = new File("src/test/resources/com/epam/javase/t02/test.txt");
+    File auxFile = new File("src/test/resources/com/epam/javase/t02/auxFile.txt");
 
     @Test (expected = NoSuchPropertiesFileException.class)
     public void shouldThrowExceptionIfFileIsNotExist() throws Exception {
-        PropertiesKeeper keeper = PropertiesKeeper.getInstance(new File(""));
+        PropertiesKeeper keeper = new PropertiesKeeper(new File(""));
     }
 
     @Test (expected = NoSuchPropertyKeyException.class)
     public void shouldThrowExceptionIfPropertiesKeyNotFound() throws Exception {
-        PropertiesKeeper keeper = PropertiesKeeper.getInstance(file);
+        PropertiesKeeper keeper = new PropertiesKeeper(file);
         keeper.getValue("value");
     }
 
     @Test
     public void shouldReturnValidValueIfKeyExists() throws Exception {
-        PropertiesKeeper keeper = PropertiesKeeper.getInstance(file);
+        PropertiesKeeper keeper = new PropertiesKeeper(file);
         String value = keeper.getValue("key");
         assertTrue(value.equals("value"));
     }
 
     @Test
     public void shouldReturnAllKeys() throws Exception {
-        PropertiesKeeper keeper = PropertiesKeeper.getInstance(file);
+        PropertiesKeeper keeper = new PropertiesKeeper(file);
         Set<String> set = keeper.getAllKeys();
         assertTrue(set.contains("key"));
         assertTrue(set.contains("another"));
+    }
+
+    @Test
+    public void shouldContainOnlyNewValueThenLoadNewFile() throws Exception {
+        PropertiesKeeper keeper = new PropertiesKeeper(file);
+        keeper.loadFile(auxFile);
+        assertTrue (keeper.getAllKeys().size() == 1);;
+        assertTrue(keeper.getAllKeys().contains("capital"));
     }
 }
