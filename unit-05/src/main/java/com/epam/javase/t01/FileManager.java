e06a39d88f2d4a43edf295dbf7b07ef2858646e6
Andrey <mailtoivanovandrey@gmail.com>
2017-03-15 04:28:35 +0300

    u05t01 - Added FileManager implementation

---
+++ unit-05/src/main/java/com/epam/javase/t01/FileManager.java
@@ -0,0 +1,63 @@
+package com.epam.javase.t01;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Represents a simple implementation of file manager.
+ *
+ * Created by aivanov on 3/15/2017.
+ */
+public class FileManager {
+
+    private File file;
+
+    public FileManager(File file) throws IOException {
+        if (!file.exists()) {
+            throw new IOException("No such file or directory");
+        }
+        this.file = file;
+    }
+
+    /**
+     * Open parent directory.
+     *
+     * @throws IOException if the parent directory doesn't exist
+     */
+    public void up() throws IOException {
+        if(file.getParentFile() == null) {
+            throw new IOException("No such file or directory.");
+        }
+
+        file = file.getParentFile();
+    }
+
+
+    public String getCurrentPath() {
+        return file.getAbsolutePath();
+    }
+
+    public File[] getDirectoryContent() {
+        return file.listFiles();
+    }
+
+    /**
+     * Open selected file or directory.
+     *
+     * @param file file or directory to be opened
+     * @throws IOException if the path doesn't exist
+     */
+    public void open(File file) throws IOException {
+        if(!file.exists()) {
+            throw new IOException("No such file or directory.");
+        }
+
+        if (file.isFile()) {
+            System.out.println("File opened: " + file.getCanonicalPath());
+        } else {
+            this.file = file;
+        }
+     }
+
+
+}
