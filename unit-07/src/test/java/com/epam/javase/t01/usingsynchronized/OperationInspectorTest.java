609030f86e35afca1826cccfc66c3c08b96a6e4d
Andrey <mailtoivanovandrey@gmail.com>
2017-03-21 00:58:29 +0300

    u07t01 - Fixed OperationInspectorTest

--- unit-07/src/test/java/com/epam/javase/t01/usingsynchronized/OperationInspectorTest.java
+++ unit-07/src/test/java/com/epam/javase/t01/usingsynchronized/OperationInspectorTest.java
@@ -2,7 +2,9 @@ package com.epam.javase.t01.usingsynchronized;
 
 import org.junit.Test;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.util.Arrays;
 
 import static junit.framework.TestCase.assertTrue;
@@ -14,6 +16,7 @@ import static junit.framework.TestCase.assertTrue;
 public class OperationInspectorTest {
 
     File path = new File("src/test/resources/com/epam/javase/t01/transfers.txt");
+    File answer = new File("src/test/resources/com/epam/javase/t01/answers.txt");
 
     @Test (expected = IllegalArgumentException.class)
     public void shouldThrowExceptionIfFileNotExist() throws Exception {
@@ -27,8 +30,23 @@ public class OperationInspectorTest {
         for (String[] s : inspector.getOrdersQueue()) {
             assertTrue(s.length == 3);
         }
-
-        inspector.getOrdersQueue().stream().forEach(s -> System.out.println(Arrays.toString(s)));
     }
 
+    @Test
+    public void checkIfResultEqualsToExpectedValues() throws Exception {
+        BufferedReader in = new BufferedReader(new FileReader(answer));
+
+        OperationInspector inspector = new OperationInspector(path);
+        inspector.run();
+
+        Thread.sleep(1000);
+
+        String s = "";
+
+        while ((s = in.readLine()) != null) {
+            int id = Integer.parseInt(s.split(",")[0]);
+            int balance = Integer.parseInt(s.split(",")[1]);
+            assertTrue(inspector.getAccountBalanceByID(id) == balance);
+        }
+    }
 }
